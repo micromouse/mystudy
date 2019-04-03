9e30969313d9e258fb8071f3def39b3114789a8b
mouse micro <eggeggak@vip.qq.com>
2019-04-03 22:24:03 +0800

    add elasticsearch

---
+++ ElasticSearchDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System;
+
+namespace ElasticSearchDemo {
+    public class Program {
+        public static void Main(string[] args) {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseSerilog((context, configuration) =>
+                {
+                    configuration
+                        .ReadFrom.Configuration(context.Configuration)
+                        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200")) {
+                            IndexDecider = (@event, offset) => "mystudy",
+                            TypeName = "log",
+                            AutoRegisterTemplate = true,
+                            AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6
+                        });
+
+                })
+                .UseStartup<Startup>();
+    }
+}
