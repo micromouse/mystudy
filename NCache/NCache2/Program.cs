08f8cb3e11b21eb50abf7e0f358a9922fe0a03d4
mouse micro <eggeggak@vip.qq.com>
2019-06-11 20:47:01 +0800

    publish NCache2

--- NCache/NCache2/Program.cs
+++ NCache/NCache2/Program.cs
@@ -19,6 +19,7 @@ namespace NCache2
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options=>options.ListenAnyIP(5000))
                 .UseStartup<Startup>();
     }
 }
