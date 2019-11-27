7d74d84d0f294b09ad67f0e4b860db1bfe5b863e
dxq <dxq@dxqstudy.com>
2019-11-27 10:50:05 +0800

    add apollodemo project

---
+++ Apollo/ApolloDemo/EventBusSetting.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace ApolloDemo {
+    public class EventBusSetting {
+        public string Message { get; set; }
+        public IList<EventBusDetailSetting> Details { get; set; }
+    }
+
+    public class EventBusDetailSetting {
+        public string Code { get; set; }
+        public string Message { get; set; }
+    }
+
+    public static class StringExtensions {
+        public static T ToDeserializer<T>(this string s) {
+            return JsonSerializer.Deserialize<T>(s);
+        }
+    }
+
+    public static class IConfigurationExtensions {
+        /// <summary>
+        /// 使用IConfiguration[key]获得值并反序列化到类型T
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="configuration"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static T GetAndDeserializer<T>(this IConfiguration configuration, string key) {
+            return configuration[key].ToDeserializer<T>();
+        }
+
+
+    }
+}
---
+++ Apollo/ApolloDemo/Program.cs
@@ -0,0 +1,28 @@
+using Com.Ctrip.Framework.Apollo;
+using Com.Ctrip.Framework.Apollo.Enums;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace ApolloDemo {
+    public class Program {
+        public static void Main(string[] args) {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureAppConfiguration(builder =>
+                    {
+                        builder
+                            .AddApollo(builder.Build().GetSection("apollo"))
+                            .AddDefault(ConfigFileFormat.Json)
+                            .AddDefault(ConfigFileFormat.Xml)
+                            .AddDefault();
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
